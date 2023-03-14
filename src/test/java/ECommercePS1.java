2ae4ae890b1f5695b4d8046f0d0bb77c54af54dc
shashank-zs <zs@92146>
2023-03-15 01:36:19 +0530

    Actions

--- src/test/java/ECommercePS1.java
+++ src/test/java/ECommercePS1.java
@@ -1,5 +1,6 @@
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.utils.excelWriter;
 import org.utils.webDriverUtils;
@@ -15,15 +16,8 @@ public class ECommercePS1 {
     @Test
 
     public void problem1() throws IOException, InvalidFormatException {
-        webDriverUtils webd =new webDriverUtils();
-        webd.browserOpen();
-        WebDriver driver = webd.driver;
-        driver.get(URL_FOR_FLIPKART);
-        HomepageFlipkart objHomepage = new HomepageFlipkart(driver);
-        objHomepage.SearchingMobilePhone("Mobile Phones");
-        SearchedMobilePage objMobiles = new SearchedMobilePage(driver);
-        Map<String, String> PhoneWithPrice = objMobiles.mobileWithPrice();
-        excelWriter dataStored= new excelWriter();
-        dataStored.excelWrite(PhoneWithPrice);
+        System.out.println("shashank");
+        String shan="jing";
+        Assert.assertEquals("jindg",shan);
     }
 }
